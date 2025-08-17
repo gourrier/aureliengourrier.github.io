34521f624b03fbc234c83b8c1a86501c08bfbdb1
gourrier <40382435+gourrier@users.noreply.github.com>
2025-08-17 15:41:39 +0000

    Deploying to gh-pages from @ gourrier/gourrier.github.io@884a1dfb792b1619b66faf93f49b84b237896765 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -426,16 +426,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-warning-finally-getting-down-to-build-a-new-website-with-useful-info-construction-this-might-take-a-while-so-please-be-patient-and-forgive-me-for-whatever-i-39-ve-done-wrong-the-contact-info-from-socials-icons-should-work-so-feel-free-to-post-a-message-wink",
+          title: ':warning: Finally getting down to build a new website with useful info :construction:...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
