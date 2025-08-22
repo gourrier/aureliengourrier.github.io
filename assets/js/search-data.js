57e763702b85dcaf84d03f7c931383f2be89a705
gourrier <40382435+gourrier@users.noreply.github.com>
2025-08-22 09:50:25 +0000

    Deploying to gh-pages from @ gourrier/gourrier.github.io@ef0f21146f443b4f58bca754b10ffbe2d2937dd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A quick dive into a few selected projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -401,46 +401,11 @@ ninja.data = [{
             },},{id: "news-finally-getting-down-to-building-a-new-website-with-useful-info-this-might-take-a-while-hourglass-so-please-be-patient-and-forgive-me-for-whatever-i-ve-done-wrong-the-contact-info-from-socials-icons-should-work-so-feel-free-to-post-a-message-mailbox-with-no-mail",
           title: 'Finally getting down to building a new website with useful info ! This...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-geosuperres-2025-29",
+          title: 'GeoSuperRes 2025-29',
+          description: "A MIAI Cluster chair on deep-learning super-resolution for imaging",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
+              window.location.href = "/projects/2025_MIAI_GeoSuperRes/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
